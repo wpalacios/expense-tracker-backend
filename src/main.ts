a2181bc5a02ad1815385c0f7d29c1d3c2cc01143
William Palacios <wfpmartinez@gmail.com>
2024-12-29 15:47:50 -0600

    add https://expenses-tracker-frontend-ten.vercel.app as origin

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: [
+      'http://localhost:3000',
+      'https://expenses-tracker-frontend-ten.vercel.app',
+    ], // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies if needed
   });
